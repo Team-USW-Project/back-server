fa3f8bbd5630a18f5f674a70f582620866fd340a
최다빈 <choidabin@choedabin-ui-MacBookPro.local>
2025-12-17 21:49:57 +0900

    init project

---
+++ src/main/java/java/com/uswproject/UswProjectApplication.java
@@ -0,0 +1,13 @@
+package java.com.uswproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UswProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UswProjectApplication.class, args);
+    }
+
+}
