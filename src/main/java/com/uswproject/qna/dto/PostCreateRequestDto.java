489d18903bf97dc2bc01941412377a68993f13d9
Yong <jk8483277@naver.com>
2026-02-03 21:58:29 +0900

    게시글 작성 코멘트반영2

--- src/main/java/com/uswproject/qna/dto/PostCreateRequestDto.java
+++ src/main/java/com/uswproject/qna/dto/PostCreateRequestDto.java
@@ -2,18 +2,16 @@ package com.uswproject.qna.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
-import lombok.Setter;
+
 
 
 @Getter
 
 public class PostCreateRequestDto {
-
-    @Setter
+    
     @NotBlank(message = "제목은 필수입니다.")
     private String title;
 
-    @Setter
     @NotBlank(message = "내용은 필수입니다.")
     private String content;
 
