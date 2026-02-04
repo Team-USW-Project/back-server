a42b9681b2f99d143ba8cf4085f66905b5c65b6c
최다빈 <choidabin@10.nate.com>
2026-02-04 16:34:57 +0900

    exception 처리 수정

--- src/main/java/com/uswProject/userLastTime/service/UserLastTimeService.java
+++ src/main/java/com/uswProject/userLastTime/service/UserLastTimeService.java
@@ -3,7 +3,9 @@ package com.uswProject.userLastTime.service;
 import com.uswProject.userLastTime.client.SearchRouteOpenFeign;
 import com.uswProject.userLastTime.dto.OdsayResponse;
 import com.uswProject.userLastTime.exception.LastTimeErrorCode;
+import com.uswProject.userLastTime.exception.LastTimeException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
@@ -11,6 +13,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserLastTimeService {
 
     private final SearchRouteOpenFeign client;
@@ -20,8 +23,7 @@ public class UserLastTimeService {
         OdsayResponse response = client.searchPath(startX, startY, endX, endY);
 
         if (response == null || response.getResult() == null || response.getResult().getPath() == null) {
-            System.out.println(LastTimeErrorCode.ROUTE_NOT_FOUND);
-            return Collections.emptyList();
+            throw new LastTimeException(LastTimeErrorCode.ROUTE_NOT_FOUND);
         }
 
         return response.getResult().getPath().stream()
