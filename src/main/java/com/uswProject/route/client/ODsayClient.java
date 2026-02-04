62bb401d54f83e734f0f60a4b215eb7e865b5e50
전원준 <jwjjwj1960@naver.com>
2026-02-04 21:21:06 +0900

    feat: FeignClient 추가
    ODsay에서 외부 api를 호출하기 위한 Client추가

---
+++ src/main/java/com/uswProject/route/client/ODsayClient.java
@@ -0,0 +1,18 @@
+package com.uswProject.route.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "odsayClient", url = "${odysay.api.base-url}")
+public interface ODsayClient {
+
+    @GetMapping("/searchPubTransPath")
+    Object getSearchPubTransPath(
+            @RequestParam("SX") String startX,
+            @RequestParam("SY") String startY,
+            @RequestParam("EX") String endX,
+            @RequestParam("EY") String endY,
+            @RequestParam("apiKey") String apiKey // 발급받은 ODsay API Key
+    );
+}
