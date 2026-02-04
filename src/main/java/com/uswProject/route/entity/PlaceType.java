b27620a985132767cd51182132d53ca0bd9e4842
전원준 <jwjjwj1960@naver.com>
2026-02-04 19:34:45 +0900

    feat: PlaceType, SegmentMode Enum 추가
    1. 경로 리스트 중 Segment라는 하나의 경로가 어떤 Mode로 이루어져 있는지 구분하는 enum
    2. 하나의 Segment 시작부터 끝까지가 어떤 장소로 되어있는지 구분하는 enum

---
+++ src/main/java/com/uswProject/route/entity/PlaceType.java
@@ -0,0 +1,9 @@
+package com.uswProject.route.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum PlaceType {
+    STATION, BUS_STOP, POINT;
+
+}
---
+++ src/main/java/com/uswProject/route/entity/SegmentMode.java
@@ -0,0 +1,8 @@
+package com.uswProject.route.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum SegmentMode {
+    WALK, BUS, SUBWAY;
+}
