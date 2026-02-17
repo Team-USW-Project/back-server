b7f42f5b0d2c10345a6fb2edce69ce275ab52ef0
전원준 <jwjjwj1960@naver.com>
2026-02-17 22:08:41 +0900

    feat: OdsayRepository search 메서드 추가
    List<RoutePlan>을 반환하는 search 메서드 추가

--- src/main/java/com/uswProject/route/repository/impl/OdsayTransitRouteRepository.java
+++ src/main/java/com/uswProject/route/repository/impl/OdsayTransitRouteRepository.java
@@ -1,12 +1,15 @@
 package com.uswProject.route.repository.impl;
 
 import com.uswProject.route.client.OdsayFeignClient;
+import com.uswProject.route.dto.odsay.OdsaySearchResponse;
 import com.uswProject.route.entity.Place;
 import com.uswProject.route.entity.RoutePlan;
 import com.uswProject.route.entity.enums.RouteOption;
+import com.uswProject.route.mapper.OdsayRouteMapper;
 import com.uswProject.route.repository.TransitRouteRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -14,9 +17,30 @@ import org.springframework.stereotype.Repository;
 public class OdsayTransitRouteRepository implements TransitRouteRepository {
 
     private final OdsayFeignClient client;
+    private final OdsayRouteMapper mapper;
+
+    @Value("${odsay.opt:0}")
+    private int defaultOpt;
 
     @Override
     public List<RoutePlan> search(Place origin, Place destination, RouteOption option) {
-        return List.of();
+        int opt = defaultOpt;
+        if (option == RouteOption.MIN_TRANSFER) {
+            opt = 1;
+        }
+
+        OdsaySearchResponse res = client.getSearchPubTransPathT(
+                origin.getLng(), origin.getLat(),
+                destination.getLng(), destination.getLat(),
+                opt,
+                0,
+                0
+        );
+
+        if (res != null && res.getError() != null) {
+            throw new IllegalStateException("Odsay error: " + res.getError().getCode() + " " + res.getError().getMessage());
+        }
+
+        return mapper.toRoutePlans(res, origin, destination);
     }
 }
